d2cd92153922fcbf104da7d41ae418bf2e2a735c
Arien Kock <arien.kock@gmail.com>
2016-12-10 17:22:38 +0100

    initial commit

---
+++ src/main/java/guru/uit/authnproxy/Application.java
@@ -0,0 +1,38 @@
+package guru.uit.authnproxy;
+
+import org.eclipse.jetty.proxy.AsyncProxyServlet;
+import org.eclipse.jetty.proxy.ProxyServlet;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Arien on 10-Dec-16.
+ */
+@SpringBootApplication
+public class Application {
+
+    @Bean
+    public ServletRegistrationBean proxyServlet(final ServletConfig dispatcherServlet, @Value("${destination.url}") String destUrl) throws ServletException {
+        ProxyServlet.Transparent proxyServlet = new ProxyServlet.Transparent();
+        final Map<String, String> params = new HashMap<String, String>();
+        params.put("proxyTo", destUrl);
+        params.put("prefix", "/proxy");
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(proxyServlet, "/proxy/*");
+        servletRegistrationBean.setAsyncSupported(true);
+        servletRegistrationBean.setName("proxyServlet");
+        servletRegistrationBean.setInitParameters(params);
+        return servletRegistrationBean;
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
